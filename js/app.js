2855c7a74071e051ee4460348528e2e5b9f614fd
makhmudoff <gachon031698@gmail.com>
2023-04-04 11:50:58 +0900

    html, css and two js file added

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const changeLocation = document.getElementById("change-location");
+const card = document.getElementById("card");
+const details = document.getElementById("details");
+const weatherIcon = document.getElementById("weather-icon");
+const overlay = document.getElementById("overlay");
